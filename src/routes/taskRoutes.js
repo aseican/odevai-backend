9444449ce80463aa3f5632823d65d81d1c51425f
aseican <emreecansever@gmail.com>
2025-11-20 10:34:35 +0300

    gereksiz src

--- src/routes/taskRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { listTasks } = require('../controllers/taskController');
-
-router.get('/', protect, listTasks);
-
-module.exports = router;
